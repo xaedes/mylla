7f7cb95932585e80c0144e5fa4e9e2fe761bd10b
xaedes <xaedes@gmail.com>
2023-04-14 20:05:06 +0200

    add parallel chat and sample from patched logits

--- mylla/main.py
+++ mylla/main.py
@@ -3,6 +3,10 @@ from mylla.llama_chat import *
 
 def main():
     chat = LlamaChat(ChatConfig(prompt_fn='chat-with-bob.txt', llama_cfg=LlamaConfig(model='13b')))
+    chat2 = LlamaChat(ChatConfig(prompt_fn='chat-with-bob2.txt', llama_cfg=LlamaConfig(model='13b')))
+
+    chat2.load_state()
+    if chat2.process_input() > 0: chat2.save_state()
 
     chat.load_state()
     if chat.process_input() > 0: chat.save_state()
@@ -30,11 +34,16 @@ def main():
                     for i in range(1, tkns.n):
                         token = tkns.tokens[i]
                         chat.insert_token(token)
+                        chat2.insert_token(token)
                     chat.process_input(echo=False)
+                    chat2.process_input(echo=False)
                 is_interacting = False
             else:
+                chat.patch_logits(chat2, 0.5)
                 token = chat.sample(ignore=["\\"])
+                chat2.insert_token(token)
             chat.process_input()
+            chat2.process_input(echo=False)
 
 
 if __name__ == "__main__": main()
